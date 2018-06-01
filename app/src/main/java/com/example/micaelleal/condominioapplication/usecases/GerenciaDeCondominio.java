54669a0bdaf26a53c9446c23041a470a63561b90
MicaelLeal <mycaellmoura@hotmail.com>
2018-06-01 03:48:16 -0300

    Implementação de classe para comunicação da interface com caso de uso

--- app/src/main/java/com/example/micaelleal/condominioapplication/usecases/GerenciaDeCondominio.java
+++ app/src/main/java/com/example/micaelleal/condominioapplication/usecases/GerenciaDeCondominio.java
@@ -4,13 +4,14 @@ import com.example.micaelleal.condominioapplication.models.Apartamento;
 import com.example.micaelleal.condominioapplication.models.Condominio;
 import com.example.micaelleal.condominioapplication.models.Despesa;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class GenrenciaDeCondominio {
+public class GerenciaDeCondominio {
 
     private List<Despesa> despesas;
 
-    public GenrenciaDeCondominio(List<Despesa> despesas) {
+    public GerenciaDeCondominio(List<Despesa> despesas) {
         this.despesas = despesas;
     }
 
@@ -18,14 +19,17 @@ public class GenrenciaDeCondominio {
         despesas.add(despesa);
     }
 
-    public void calcularCondominio(String mesAno) {
+    public List<Condominio> calcularCondominio(String mesAno) {
 
+        List<Condominio> condominios = new ArrayList<>();
         for (Despesa despesa : despesas) {
             for (Apartamento apt : despesa.getApartamentos()) {
                 Condominio condominio = apt.novoCondominio(mesAno);
                 condominio.adicionarDespesa(despesa);
+                condominios.add(condominio);
             }
         }
+        return condominios;
     }
 
 }
