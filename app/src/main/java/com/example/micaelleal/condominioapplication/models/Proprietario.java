0da29a55f2f99c6dfc72c619b39b7ad81d8b9cfe
FilipeNeiva <filipevilanovaneiva@gmail.com>
2018-06-01 11:29:44 -0300

    alterações na classe proprietario

--- app/src/main/java/com/example/micaelleal/condominioapplication/models/Proprietario.java
+++ app/src/main/java/com/example/micaelleal/condominioapplication/models/Proprietario.java
@@ -1,4 +1,12 @@
 package com.example.micaelleal.condominioapplication.models;
 
 public class Proprietario {
+
+    private String nome;
+    private String telefone;
+
+    public Proprietario(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
 }
